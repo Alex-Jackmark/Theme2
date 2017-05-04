caf88e77549c24245e6b2343eb5e67f6b114d6c9
alex.mellor@jackmark.com <alex.mellor@jackmark.com>
2017-05-04 12:26:48 +0100

    minor CSS and js change.

--- js/ClickTest.js
+++ js/ClickTest.js
@@ -3,8 +3,8 @@ jQuery(document).ready(function () {
 		//jQuery("#cart-container").toggle();
 		jQuery("#meta-container").toggleClass("collapsedSmall");
 		jQuery("#meta-subContainer").toggleClass("dropped");
-		jQuery(".fa").toggleClass("fa-chevron-up");
-		jQuery(".fa").toggleClass("fa-chevron-down");
+		jQuery(".fa:first").toggleClass("fa-chevron-up");
+		jQuery(".fa:first").toggleClass("fa-chevron-down");
 		jQuery(".tester").toggleClass("collapsedLarge");
 		jQuery(".myAccountItem").toggleClass("hiddenItem");
 		jQuery(".logOutItem").toggleClass("hiddenItem");
